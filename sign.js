d0be45a9f61654259ebfd22e293faef5446a7925
TYoung1 <lail_mind@naver.com>
2023-03-08 17:44:39 +0900

    fir,sec commit

---
+++ sign.js
@@ -0,0 +1,68 @@
+let num= parseInt(Math.random()*10000)
+function blurIt(num){
+    var i;
+    
+    var danger = document.getElementsByClassName("dan");
+    var ival = event.currentTarget.value;
+    
+    if(ival==''){
+   document.getElementById(num).style.display= "block"; 
+    }
+    else{
+        document.getElementById(num).style.display= "none"; 
+    }
+    
+}
+
+function rannum(){
+    console.log(num);
+}
+function inputChk(){
+    const idreg= /^(?=.*[A-Za-z])(?=.*\d)(?=.*[_!-])[A-Za-z\d_!-]{8,}$/;
+    const pwreg= /^(?=.*[A-Za-z])(?=.*\d)(?=.*[_!-])[A-Za-z\d_!-]{8,}$/;
+    let id = document.getElementById("id").value;
+    let pw = document.getElementById("pw").value;
+    let pwchk = document.getElementById("pwchk").value;
+    let name = document.getElementById("name");
+    let year = document.getElementById("year");
+    let month = document.getElementById("mon");
+    let email = document.getElementById("email").value;
+    let gen = document.getElementById("gender").value;
+    let nat = document.getElementById("lo").value;
+    let toc = document.getElementById("tochk").value;
+    if(!idreg.test(id)){
+        document.getElementById("fir").innerText="아이디는 영소대문자,숫자, _ , - , !만사용가능합니다";
+        document.getElementById("fir").style.display="block";
+    }else{
+        if(!pwreg.test(pw)){
+            document.getElementById("sec").innerText="비밀번호는 영소대문자,숫자, _ , - , !만사용가능합니다";
+            document.getElementById("sec").style.display="block";
+        }else{
+        if(pw!=pwchk){
+            document.getElementById("thi").innerText="비밀번호가 일치하지 않습니다.";
+            document.getElementById("thi").style.display="block";
+        }
+        else{
+            if(gen==""){
+                alert("성별을 선택해주세요.");
+            }
+            else{
+                if(!email.indexOf("@") || email!=""){
+                    alert("이메일의 형식이 아닙니다.")
+                }
+                else{
+                    if(num!=toc){
+                        alert("인증번호가 일치하지 않습니다.");
+                    }
+                    else{
+                        alert("회원가입이 완료되었습니다");
+                        location.reload();
+                    }
+                }
+                
+            }
+        }
+    }
+    
+}
+}
---
+++ test.js
@@ -0,0 +1,5 @@
+// function CheckValue(){
+//     var aa= document.getElementById("chk1");
+//     var bb= document.getElementById("chk2");
+//     bb.checked = aa.checked == true? true:false;
+// }
