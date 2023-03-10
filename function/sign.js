c449ede3f7b43d6e1282fefe63e1bbe4b550eb18
TYoung1 <lail_mind@naver.com>
2023-03-10 17:14:35 +0900

    add birth

--- function/sign.js
+++ function/sign.js
@@ -26,6 +26,7 @@ function rannum(){
     }
 }
 function inputChk(){
+    const numreg =/^[0-9]+$/;
     // 정규식 하나이상의 문자, 숫자 , _!-중한개  , 8글자 이상
     const idreg= /^(?=.*[A-Za-z])(?=.*\d)(?=.*[_!-])[A-Za-z\d_!-]{8,}$/;
     const pwreg= /^(?=.*[A-Za-z])(?=.*\d)(?=.*[_!-])[A-Za-z\d_!-]{8,}$/;
@@ -33,12 +34,14 @@ function inputChk(){
     let pw = document.getElementById("pw").value;
     let pwchk = document.getElementById("pwchk").value;
     let name = document.getElementById("name");
-    let year = document.getElementById("year");
+    let year = document.getElementById("year").value;
+    let day = document.getElementById("day").value;
     let month = document.getElementById("mon");
     let email = document.getElementById("email").value;
     let gen = document.getElementById("gender").value;
     let nat = document.getElementById("lo").value;
     let toc = document.getElementById("tochk").value;
+    
     // 정규식 테스트
     if(!idreg.test(id)){
         document.getElementById("fir").innerText="아이디는 영소대문자1개이상,숫자1개이상, _ , - , !1개이상(8글자이상)으로 생성하여주세요";
@@ -55,11 +58,17 @@ function inputChk(){
             document.getElementById("thi").style.display="block";
         }
         else{
+             if(!numreg.test(year)&& !numreg.test(day)){
+                document.getElementById("birth").style.display="block";
+             }
+             else{
+
             // 성별 선택여부 확인
             if(gen==""){
                 alert("성별을 선택해주세요.");
             }
             else{
+                
                 // 이메일에 @ 가안들어가면 형식 안맞는거로판단 , (선택)이기때문에 이메일을 입력햇을떄만 검사
                 if(!email.indexOf("@") && email!=""){
                     alert("이메일의 형식이 아닙니다.")
@@ -77,6 +86,7 @@ function inputChk(){
                 }
                 
             }
+        }
         }
     }
     
